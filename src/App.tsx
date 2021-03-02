4b699a03d69e8f4880de84abc2b0d38be2419c04
Edrees <s.i.sarwari@hotmail.com>
2021-03-03 00:17:34 +0100

    optimization

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { DataGrid } from '@material-ui/data-grid';
-import axios from 'axios';
-import './App.css';
+import React from 'react';
+import DataGridComponent from './DataGridComponent/';
+import { makeStyles } from '@material-ui/core/styles';
+import './App.scss';
 
-function App() {
-  const API =
-    'https://api.coinstats.app/public/v1/coins?skip=0&limit=5&currency=USD';
+const useStyles = makeStyles({
+  root: {
+    color: "#fff"
+  },
+});
 
-  let [responseData, setResponseData] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get(API)
-      .then((response) => {
-        setResponseData(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
-
-  console.log('dadad', responseData);
-
-  const rows = [
-    { id: 1, col1: "Hello", col2: "World" },
-    { id: 2, col1: "XGrid", col2: "is Awesome" },
-    { id: 3, col1: "Material-UI", col2: "is Amazing" },
-    { id: 4, col1: "Hello", col2: "World" },
-    { id: 5, col1: "XGrid", col2: "is Awesome" },
-    { id: 6, col1: "Material-UI", col2: "is Amazing" }
-  ];
-
-  const columns = [
-    { field: 'col1', headerName: 'Column 1', width: 150 },
-    { field: 'col2', headerName: 'Column 2', width: 150 },
-  ];
-
-  // const sortModel = [
-  //   {
-  //     field: 'username',
-  //     sort: 'asc',
-  //   },
-  // ];
+const App = () => {
+  const classes = useStyles();
 
   return (
-    <div className="App">
-      <header className="App-header">
-        Crypto Currencies
-      </header>
-      <div style={{ height: 400, width: '100%' }}>
-        <DataGrid  rows={rows} columns={columns} />
-      </div>
+    <div className={classes.root}>
+      <header className="cs__header">Crypto Currencies</header>
+      <DataGridComponent />
     </div>
   );
 }
---
+++ src/DataGridComponent/index.tsx
@@ -0,0 +1,136 @@
+import React, { useEffect, useState } from 'react';
+import { DataGrid } from '@material-ui/data-grid';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import axios from 'axios';
+import './styles.scss';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    height: '638px',
+  },
+  tableHeader: {
+    color: "#ffff99",
+    fontWeight: 'bold',
+  },
+  iconWidth: {
+    '& img': {
+      width: theme.spacing(3),
+    },
+  },
+  positive: {
+    color: '#15ce34',
+    fontWeight: 'bold',
+  },
+  negative: {
+    color: '#ff0000',
+    fontWeight: 'bold',
+  },
+}));
+
+const DataGridComponent = () => {
+  const classes = useStyles();
+
+  const API: string = 'https://api.coinstats.app/public/v1/coins?skip=0&limit=100&currency=USD';
+
+  let [responseData, setResponseData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(API)
+      .then((response) => {
+        setResponseData(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  let coinRows: any = [];
+
+  for (let key in responseData) {
+    if (responseData.hasOwnProperty(key)) {
+      coinRows = responseData[key];
+    }
+  }
+
+  const columns = [
+    {
+      field: 'rank',
+      headerName: 'Rank',
+      headerClassName: classes.tableHeader,
+      width: 90,
+    },
+    {
+      field: 'name',
+      headerName: 'Coin Name',
+      headerClassName: classes.tableHeader,
+      width: 150,
+    },
+    {
+      field: 'icon',
+      headerName: 'Icon',
+      headerClassName: classes.tableHeader,
+      width: 90,
+      cellClassName: classes.iconWidth,
+      renderCell: (params: any) => <img src={params.value} alt={params.value} />,
+    },
+    {
+      field: 'symbol',
+      headerName: 'Symbol',
+      headerClassName: classes.tableHeader,
+      width: 120
+    },
+    {
+      field: 'price',
+      headerName: 'Price',
+      headerClassName: classes.tableHeader,
+      width: 120,
+      renderCell: (params: any) => (
+        <strong>
+          $
+          {params.value > 0.09
+            ? (Math.round(params.value * Math.pow(10, 2)) / Math.pow(10, 2)).toFixed(2)
+            : params.value.toFixed(6)}
+        </strong>
+      ),
+    },
+    {
+      field: 'priceChange1h',
+      headerName: '1H Report',
+      headerClassName: classes.tableHeader,
+      width: 150,
+      renderCell: (params: any) =>
+        params.value > 0 ? (
+          <div className={classes.positive}>{params.value}%</div>
+        ) : (
+            <div className={classes.negative}>{params.value}%</div>
+          ),
+    },
+    {
+      field: 'priceChange1w',
+      headerName: '1W Report',
+      headerClassName: classes.tableHeader,
+      width: 150,
+      renderCell: (params: any) =>
+        params.value > 0 ? (
+          <div className={classes.positive}>{params.value}%</div>
+        ) : (
+            <div className={classes.negative}>{params.value}%</div>
+          ),
+    },
+  ];
+
+  return (
+    <div className={classes.root}>
+      <DataGrid
+        className="cs__table"
+        pageSize={10}
+        rowsPerPageOptions={[10, 50, 100]}
+        rows={coinRows}
+        columns={columns}
+      />
+    </div>
+  );
+};
+
+export default DataGridComponent;
